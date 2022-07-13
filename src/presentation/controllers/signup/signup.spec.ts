08a315c1fa17e137ec0aa4e04adee5377719c550
demetriusleonardobantim <demetriusbantimvas@gmail.com>
2022-07-12 22:15:50 -0300

    test: ensure SignUpController returns 200 if valid data is provided

--- src/presentation/controllers/signup/signup.spec.ts
+++ src/presentation/controllers/signup/signup.spec.ts
@@ -202,3 +202,17 @@ describe('SignUp Controller', () => {
     })
   })
 })
+
+test('Should return 200 if valid data is provided', () => {
+  const { sut } = makeSut()
+  const httpRequest = {
+    body: {
+      name: 'valid_name',
+      email: 'valid_email@mail.com',
+      password: 'valid_password',
+      passwordConfirmation: 'valid_password'
+    }
+  }
+  const httpResponse = sut.handle(httpRequest)
+  expect(httpResponse.statusCode).toBe(200)
+})
